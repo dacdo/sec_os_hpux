bd613e5741119caafd880aec7ca1c9da182fa81a
DacDo <mr.dacdo@gmail.com>
2019-06-12 10:01:08 +0800

    typo

--- controls/section_2_1_11.rb
+++ controls/section_2_1_11.rb
@@ -6,7 +6,7 @@ control '2.1.1.11 Disable inetd instl_boots for udp' do
   title '2.1.1.11 Disable inetd instl_boots for udp'
   desc 'Disable inetd instl_boots for udp'
   describe file('/etc/inetd.conf') do
-    its('content') { should match(/#\s*instl_boots\s+?dgram\s+?udp\s+?wait\s+?root\s+?\/opt\/ignite/\/lbin\/instl_bootd/) }
+    its('content') { should match(/#\s*instl_boots\s+?dgram\s+?udp\s+?wait\s+?root\s+?\/opt\/ignite\/lbin\/instl_bootd/) }
   end
 end
 
